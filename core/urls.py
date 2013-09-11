a213b277d168acfd5ef49c4abcb94c6fcf9dfc57
alrusdi <alrusdi@gmail.com>
2013-09-11 20:31:32 +0600

    Watermarking video thumbs with play button

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import patterns, include, url, static
 from django.conf import settings
 from django.contrib import admin
 from ticketing.views import GetTicket, GetVideos, ViewVideoByTicket, stream_video, stream_mp4
+from videostore.views import watermark
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^media/wmthumbs/(?P<file>.*)$', watermark, name='make_watermark'),
     url(r'^video/(?P<ticket>[0-9a-f]+)/$', ViewVideoByTicket.as_view(), name='video_by_ticket'),
     url(r'^stream/(?P<ticket>[0-9a-f]+)/$', stream_video, name='video_stream'),
     url(r'^streammp4/(?P<ticket>[0-9a-f]+)/$', stream_mp4, name='video_streammp4'),
--- videostore/views.py
+++ videostore/views.py
@@ -1,1 +1,33 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+import os
+from PIL import Image, ImageEnhance
+from django.conf import settings
+from django.http import HttpResponse
+
+
+def watermark(request, file):
+    fname = os.path.basename(file)
+    res_path = os.path.join(settings.MEDIA_ROOT, 'wmthumbs', fname)
+
+    if not os.path.exists(res_path):
+        mark = Image.open(os.path.join(settings.ROOT_PATH, 'static', 'images', 'play.png'))
+        im = Image.open(os.path.join(settings.MEDIA_ROOT, 'thumbs', fname))
+
+        ratio = float(mark.size[0]) / float(mark.size[1])
+        w = float(im.size[0]) / 100.0 * 30
+        h = int(w / ratio)
+        mark = mark.resize((int(w), h), Image.ANTIALIAS)
+        if im.mode != 'RGBA':
+            im = im.convert('RGBA')
+        layer = Image.new('RGBA', im.size, (0,0,0,0))
+        position = (im.size[0] / 2 - mark.size[0] / 2, im.size[1] / 2 - mark.size[1] / 2)
+        layer.paste(mark, position)
+
+        Image.composite(layer, im, layer).save(res_path)
+
+    response = HttpResponse()
+    response['X-Sendfile'] = res_path
+    response['Content-Type'] = ''
+
+    return response
+
